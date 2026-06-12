fe698fffba9f1f14c9d5b22f781c54ee496cf6fc
rathishree11 <rathishreesangeetha_bit28@mepcoeng.ac.in>
2026-06-12 09:45:58 +0530

    Create calculator_project.py

---
+++ calculator_project.py
@@ -0,0 +1,26 @@
+print("Simple Calculator")
+while True:
+    print("\n1.Add")
+    print("2.Subtract")
+    print("3.Multiply")
+    print("4.Divide")
+    print("5.Exit")
+    choice = input("Enter choice: ")
+    if choice == "5":
+        print("Thank You!")
+        break
+    a = float(input("Enter first number: "))
+    b = float(input("Enter second number: "))
+    if choice == "1":
+        print("Result =", a + b)
+    elif choice == "2":
+        print("Result =", a - b)
+    elif choice == "3":
+        print("Result =", a * b)
+    elif choice == "4":
+        if b != 0:
+            print("Result =", a / b)
+        else:
+            print("Cannot divide by zero")
+    else:
+        print("Invalid Choice")
